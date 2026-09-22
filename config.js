deac26a126175c5e7043a197d458385d7ecb4f9b
kejedot <kegedung@gmail.com>
2026-09-22 12:44:26 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+// ==========================================
+// KONFIGURASI ACARA
+// ==========================================
+
+
+const EVENT_CONFIG = {
+
+
+    namaAplikasi:
+    "PRESENSI DIGITAL",
+
+
+    namaAcara:
+    "Workshop Sekretaris"
+    "DPD PKS se-Jawa Tengah 2026",
+
+
+    tanggal:
+    "22 September 2026",
+
+
+    lokasi:
+    "Khas Hotel - Semarang"
+
+
+};
